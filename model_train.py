f8f32bbe967eb244684db2a323ad8101be210058
chaeminsu <chaemiinsu@gmail.com>
2024-02-02 21:46:38 +0900

    [Feat][채민수] main 함수 arg parser 추가

--- model_train.py
+++ model_train.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from transformers import HfArgumentParser, TrainingArguments
 
 @dataclass
 class ModelArguments:
@@ -12,7 +13,19 @@ class PeftLoraArguments:
     lora_dropout: float = 0.1
 
 @dataclass
-class TrainArguments:
+class DataArguments:
     dataset_path: str = None
     max_train_samples: int = None # 디버깅 또는 빠른 train이 필요할 경우를 위해
     max_eval_samples: int = None
+
+
+def main():
+    parser = HfArgumentParser((
+        ModelArguments,
+        PeftLoraArguments,
+        DataArguments,
+        TrainingArguments
+    ))
+
+    model_args, peft_lora_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
