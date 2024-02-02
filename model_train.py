21ba7db89ce61dd2bfd7dcf0ee337bdcedc1d17e
chaeminsu <chaemiinsu@gmail.com>
2024-02-02 21:29:46 +0900

    [Feat][채민수] PEFT args 클래스 생성

--- model_train.py
+++ model_train.py
@@ -4,3 +4,9 @@ from dataclasses import dataclass
 class ModelArguments:
     model_name_or_path: str = None
     max_length: int = 256
+
+@dataclass
+class PeftLoraArguments:
+    r: int = 8
+    lora_alpha: int = 32
+    lora_dropout: float = 0.1
