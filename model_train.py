c91f49550b61f72fd02f3b05313e59d8a381887d
chaeminsu <chaemiinsu@gmail.com>
2024-02-02 21:36:30 +0900

    [Feat][채민수] Train args 클래스 생성

--- model_train.py
+++ model_train.py
@@ -10,3 +10,9 @@ class PeftLoraArguments:
     r: int = 8
     lora_alpha: int = 32
     lora_dropout: float = 0.1
+
+@dataclass
+class TrainArguments:
+    dataset_path: str = None
+    max_train_samples: int = None # 디버깅 또는 빠른 train이 필요할 경우를 위해
+    max_eval_samples: int = None
