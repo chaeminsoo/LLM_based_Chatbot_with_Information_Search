ff155e99393d2cdf1b9450541cf2b6c4fd9b67ac
chaeminsu <chaemiinsu@gmail.com>
2024-02-04 21:20:35 +0900

    [Feat][채민수] hyperparameter 수정

--- run_model_train.sh
+++ run_model_train.sh
@@ -1,27 +1,16 @@
-accelerate launch model_train.py\
+accelerate launch model_train_2.py\
     --model_name_or_path "EleutherAI/polyglot-ko-5.8b" \
     --dataset_path "{데이터 경로}" \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 16 \
     --gradient_accumulation_steps 1\
-    --eval_accumulation_steps 1 \
     --learning_rate 1e-4 \
     --optim "paged_adamw_8bit"\
-    --max_length 256 \
     --r 8 \
     --lora_dropout 0.05 \
     --lora_alpha 32 \
-    --eval_step 100 \
     --adam_beta1 0.9 \
     --adam_beta2 0.99 \
-    --max_eval_sample 100 \
-    --lr_scheduler_type "linear" \
     --bf16 true \
-    --warmup_ratio 0.06 \
-    --do_train \
-    --output_dir "{train 결과 저장 경로}" \
-    --save_total_limit 1 \
-    --load_best_model_at_end \
-    --save_strategy 'steps' \
-    --evaluation_strategy "steps"
+    --output_dir "{모델 저장 경로}" \
